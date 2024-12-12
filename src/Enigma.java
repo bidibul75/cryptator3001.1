d7632703fe8ef62af5788aeafd3f6aa0729c287a
lennyCoding <lenny.xiong@edu.esiee-it.fr>
2024-12-12 16:04:14 +0100

    added start function + todo: fix wheels not affecting output

--- src/Enigma.java
+++ src/Enigma.java
@@ -3,10 +3,120 @@ package src;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class Enigma {
 
-    private final char[] alphabet = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+    private static final char[] alphabet = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+
+    /**
+     * @throws Exception
+     */
+    public static void Start() throws Exception {
+        System.out.println("\n---ENIGMA---------------------------");
+        Scanner scanner = new Scanner(System.in);
+
+        // choice for the user
+        String choice, messageToEncrypt = "", messageToDecrypt = "", errorMessage = "";
+        while (true) {
+            System.out.print("ENIGMA - do you want to encrypt (E) or decrypt (D) ? : ");
+            choice = scanner.nextLine();
+            if (choice.equalsIgnoreCase("E") || choice.equalsIgnoreCase("D")) break;
+            else System.out.println("Please answer correctly to the question.");
+        }
+
+        while (choice.equalsIgnoreCase("E")) {
+            int[] wheel = {0, 0, 0};
+            while (true) {
+                System.out.print("ENIGMA - Which position for the first wheel (0 - 26)? : ");
+                choice = scanner.nextLine();
+                if (isValidNumber(choice)) {
+                    wheel[0] = Integer.parseInt(choice);
+                    break;
+                }
+                else System.out.println("Please answer correctly to the question.");
+            }
+
+            while (true) {
+                System.out.print("ENIGMA - Which position for the first wheel (0 - 26)? : ");
+                choice = scanner.nextLine();
+                if (isValidNumber(choice)) {
+                    wheel[1] = Integer.parseInt(choice);
+                    break;
+                }
+                else System.out.println("Please answer correctly to the question.");
+            }
+
+            while (true) {
+                System.out.print("ENIGMA - Which position for the first wheel (0 - 26)? : ");
+                choice = scanner.nextLine();
+                if (isValidNumber(choice)) {
+                    wheel[2] = Integer.parseInt(choice);
+                    break;
+                }
+                else System.out.println("Please answer correctly to the question.");
+            }
+
+            System.out.print("Please enter the message to encrypt (minuscule letters only) : ");
+            messageToEncrypt = scanner.nextLine();
+            messageToEncrypt = messageToEncrypt.trim();
+
+            if (Pattern.matches("^[a-z]+$", messageToEncrypt)) {
+                System.out.println("Message to encrypt : " + messageToEncrypt);
+                System.out.println("Encrypted message : " + Enigma.encryptEnigma(messageToEncrypt, wheel));
+                break;
+            }
+
+            System.out.println("Error : the message to encrypt is not only composed of minuscule letters");
+        }
+
+        while (choice.equalsIgnoreCase("D")) {
+            int[] wheel = {0, 0, 0};
+            while (true) {
+                System.out.print("ENIGMA - Which position for the first wheel (0 - 26)? : ");
+                choice = scanner.nextLine();
+                if (isValidNumber(choice)) {
+                    wheel[1] = Integer.parseInt(choice);
+                    break;
+                }
+                else System.out.println("Please answer correctly to the question.");
+            }
+
+            while (true) {
+                System.out.print("ENIGMA - Which position for the first wheel (0 - 26)? : ");
+                choice = scanner.nextLine();
+                if (isValidNumber(choice)) {
+                    wheel[2] = Integer.parseInt(choice);
+                    break;
+                }
+                else System.out.println("Please answer correctly to the question.");
+            }
+
+            while (true) {
+                System.out.print("ENIGMA - Which position for the first wheel (0 - 26)? : ");
+                choice = scanner.nextLine();
+                if (isValidNumber(choice)) {
+                    wheel[0] = Integer.parseInt(choice);
+                    break;
+                }
+                else System.out.println("Please answer correctly to the question.");
+            }
+
+            System.out.print("Please enter the message to decrypt (minuscule letters only) : ");
+            messageToEncrypt = scanner.nextLine();
+            messageToEncrypt = messageToEncrypt.trim();
+
+            if (Pattern.matches("^[a-z]+$", messageToEncrypt)) {
+                System.out.println("Message to decrypt : " + messageToEncrypt);
+                System.out.println("Encrypted message : " + Enigma.decryptEnigma(messageToEncrypt, wheel));
+                break;
+            }
+
+            System.out.println("Error : the message to encrypt is not only composed of minuscule letters");
+        }
+
+    }
 
     /**
      * encrypts the message given with the enigma algorithm
@@ -14,12 +124,12 @@ public class Enigma {
      * @param wheelPositions array with the positions of each wheel in order, technically would work with any size
      * @return encrypted message
      */
-    String encryptEnigma(String message, int[] wheelPositions) {
+    public static String encryptEnigma(String message, int[] wheelPositions) {
         char[] data = sanitizeMessage(message.toLowerCase().toCharArray());
 
         // run the wheel offsets as many times as we want
         for (int position : wheelPositions) {
-            for (int j = 0; j < message.length(); j++) {
+            for (int j = 0; j < data.length; j++) {
                 data[j] = getLetterWithOffset(data[j], position, false);
             }
         }
@@ -29,7 +139,7 @@ public class Enigma {
 
         // re-run the wheels again
         for (int position : wheelPositions) {
-            for (int j = 0; j < message.length(); j++) {
+            for (int j = 0; j < data.length; j++) {
                 data[j] = getLetterWithOffset(data[j], position, false);
             }
         }
@@ -37,12 +147,12 @@ public class Enigma {
         return new String(data);
     }
 
-    String decryptEnigma(String message, int[] wheelPositions) {
+    public static String decryptEnigma(String message, int[] wheelPositions) {
         char[] data = message.toLowerCase().toCharArray();
 
         // re-run the wheel offsets in reverse order (should be the same input as encoding function call)
         for (int i = wheelPositions.length - 1; i >= 0; i--) {
-            for (int j = 0; j < message.length(); j++) {
+            for (int j = 0; j < data.length; j++) {
                 // reverse the offset
                 data[j] = getLetterWithOffset(data[j], wheelPositions[i], true);
             }
@@ -53,7 +163,7 @@ public class Enigma {
 
         // same thing as above to completely reverse the algorithm
         for (int i = wheelPositions.length - 1; i >= 0; i--) {
-            for (int j = 0; j < message.length(); j++) {
+            for (int j = 0; j < data.length; j++) {
                 data[j] = getLetterWithOffset(data[j], wheelPositions[i], true);  // Inverse offset for decryption
             }
         }
@@ -68,7 +178,8 @@ public class Enigma {
      * @param inverse whether to reverse the offset (used for decryption)
      * @return letter at the specified offset
      */
-    char getLetterWithOffset(char letter, int offset, boolean inverse) {
+    public static char getLetterWithOffset(char letter, int offset, boolean inverse) {
+        System.out.println(offset);
         int firstIndex = firstIndexOfChar(alphabet, letter);
         if (inverse) {
             // subtract the offset and wrap around if needed
@@ -83,7 +194,7 @@ public class Enigma {
      * mirrors on the alphabet each letter of the character list
      * @param charList list of characters we want to mirror
      */
-    void mirrorCharList(char[] charList) {
+    public static void mirrorCharList(char[] charList) {
         for (int i = 0; i < charList.length; i++) {
             int firstIndex = firstIndexOfChar(alphabet, charList[i]);
 
@@ -99,7 +210,7 @@ public class Enigma {
      * @param character character to check
      * @return index of the first instance of the character
      */
-    int firstIndexOfChar(char[] charList, char character) {
+    public static int firstIndexOfChar(char[] charList, char character) {
         int number = -1; // if the element is not in the list
         for (int i = 0; i < charList.length; i++) {
             if (charList[i] == character) {
@@ -110,7 +221,7 @@ public class Enigma {
         return number;
     }
 
-    char[] sanitizeMessage(char[] message) {
+    public static char[] sanitizeMessage(char[] message) {
         // dynamic list of characters
         List<Character> cleanMessageData = new ArrayList<>();
         // iterate over each
@@ -128,5 +239,13 @@ public class Enigma {
             cleanMessage[i] = cleanMessageData.get(i);
         }
         return cleanMessage;
+    }
+
+    public static boolean isValidNumber(String str) {
+        if (str.matches("^[0-9]{1,2}$")) {
+            int number = Integer.parseInt(str);
+            return number >= 0 && number <= 25;
+        }
+        return false;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,40 +1,23 @@
 package src;
-import src.Enigma;
 
 
 import src.Menu;
 
 public class Main {
     // entry point
+    public static void main(String[] args) throws Exception {
+        Menu menu = new Menu();
+        // Enigma
+        Enigma.Start();
+        // End of Enigma
 
-    public static void main(String[] args) {
-       Menu menu = new Menu();
-            // Polybe
-            Polybe.Start();
-            // End of Polybe
-
-            // RC4
-            RC4.Start();
-            // End of RC4
-        Menu menu = new src.Menu();
-        Enigma enigma = new Enigma();
-//                // Polybe
-//        String sbPolybe = "Walter";
-//        System.out.println(sbPolybe);
-//        System.out.println(Polybe.encryption(sbPolybe));
-//        System.out.println(Polybe.decryption("t66"));
+        // Polybe
+        Polybe.Start();
         // End of Polybe
 
-        // Enigma test code
-        int[] wheels = {1, 3};
-        String encoded = enigma.encryptEnigma("rerewre", wheels);
-        System.out.println(encoded);
-        String decoded = enigma.decryptEnigma(encoded, wheels);
-        System.out.println(decoded);
-
-    public static void main(String[] args) throws Exception {
-       
-
+        // RC4
+        RC4.Start();
+        // End of RC4
 
     }
 }
